1a10ce911597681cb0b054ba310fa1b6ab2898eb
Faust Oswald <114805447+Oswald-Faust@users.noreply.github.com>
2023-03-01 16:52:11 +0100

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed(".typing",{
+    strings:["", "Web Developer", "Web Designer", "Web Writer", "Podcaster", "UX Writer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+}) 
